c84772579629fc98e409f8856632496da3b03d8d
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-23 22:36:25 +0530

    Environment Variables

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
