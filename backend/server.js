446d0b0b0909b8b554ddcec0f095fd20a0757264
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-23 22:40:49 +0530

    First Route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "get goals" });
+});
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
