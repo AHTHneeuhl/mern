01c72a91b1c8b6d8144cd4e52d0d84862dfa9cd1
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-23 22:48:41 +0530

    Routes File

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+const goalRouter = require("./routes/goalRoutes");
+
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "get goals" });
-});
+app.use("/api/goals", goalRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
