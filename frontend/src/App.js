c48b1f4dd5eb9305d83354ba3fd993b4998c00af
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-26 19:58:26 +0530

    Pages component and routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Register, Login, Dashboard } from "./pages";
+
 const App = () => {
-  return null;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default App;
