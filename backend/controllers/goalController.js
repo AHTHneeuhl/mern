a6808e32ee9cac22ce648710ba140da31286f653
Shubham Patrick <ahthneeuhl@gmail.com>
2022-06-26 17:20:42 +0530

    Set specific user goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -23,7 +23,7 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
-  const goal = await Goal.create({ text: req.body.text });
+  const goal = await Goal.create({ text: req.body.text, user: req.user.id });
 
   res.status(200).json(goal);
 });
